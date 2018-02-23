93de9fe4cc035bc89a52908909115b7a0be48760
Manuel Perez <manugijon@gmail.com>
2018-02-23 16:54:29 +0100

    Refactor main.py to use definitions.py file.

--- TestRound/main.py
+++ TestRound/main.py
@@ -1,6 +1,7 @@
+from definitions import *
 from util import util
 
-R, C, L, H, pizza = util.parse("input_data/example.in")
+R, C, L, H, pizza = util.parse(INPUT_DATA_DIR + "small.in")
 
 print([R, C, L, H])
 
