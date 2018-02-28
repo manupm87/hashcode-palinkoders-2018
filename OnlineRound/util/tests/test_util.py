5ed054a6a7eba0832025b35def097e66d0cc20fe
Manuel Perez <manugijon@gmail.com>
2018-02-28 15:30:27 +0100

    Complete Online Round bootstrap.

---
+++ OnlineRound/util/tests/test_util.py
@@ -0,0 +1,8 @@
+from util import util
+from definitions import *
+
+
+def test_parse_pizza():
+    r, c, l, h, pizza = util.parse(INPUT_DATA_DIR + "dummy_example.in")
+    assert [r, c, l, h] == [3, 5, 1, 6]
+    assert pizza.__eq__(["TTTTT", "TMMMT", "TTTTT"])
